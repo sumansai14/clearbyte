8bde651d0471d68351a80081e30a2054834e7206
Sai Suman Prayaga <suman.sai14@gmail.com>
2017-07-31 08:41:32 +0530

    changed procfile

--- clearbyte/settings/staging.py
+++ clearbyte/settings/staging.py
@@ -9,7 +9,16 @@ class Settings(CommonSettings):
     # STATICFILES_DIRS = (
     #     os.path.join(CommonSettings.DJANGO_ROOT, 'static'),
     # )
-
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'd6sr8sa4e51f4t',
+            'USER': 'kkeifrcmkzeazw',
+            'PASSWORD': '75cb56abdbcdb1d72a0d0d65d4b52c3f8e810dcdfb83303c9aad2faa28413818',
+            'HOST': 'ec2-23-23-225-12.compute-1.amazonaws.com',
+            'PORT': '',
+        }
+    }
     MIDDLEWARE_CLASSES = CommonSettings.MIDDLEWARE_CLASSES + ('whitenoise.middleware.WhiteNoiseMiddleware',)
 
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
